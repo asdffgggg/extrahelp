f5ccd546607fc1cc7e8d4061bcaa890fb4e3f165
asdffgggg <shubisahni@icloud.com>
2026-08-08 12:58:00 -0700

    anchor + css

--- maine.py
+++ maine.py
@@ -1,23 +1,36 @@
-from fastapi import FastAPI 
-from fasthtml.common import *
 import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fasthtml.common import *
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="."))
+
 
 @app.get("/")
 async def root():
-    return to_xml(Html(
-        Head(Title("ExtraHelp")),
+    conteant = Html(
+        Head(
+            Link(rel="stylesheet", href="static/stile.css"),
+            Title("ExtraHelp"),
+        ),
         Body(
-            P("but not you")
-        )
-    ))
+            Div(
+                H1("ExtraHelp"),
+                H3("Helping teens volunteer!"),
+                A("Get Started ➙", href = "https://www.nytimes.com/games/wordle/index.html?eafs_enabled=false"),
+                id="content",
+            )
+        ),
+    )
+    return HTMLResponse(to_xml(conteant))
 
 
 if __name__ == "__main__":
     uvicorn.run(
         "maine:app",
-        host = "127.0.0.1",
-        port = 8000,
-        reload = True,
+        host="127.0.0.1",
+        port=8000,
+        reload=True,
     )
